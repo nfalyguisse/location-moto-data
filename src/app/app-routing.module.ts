7a91f222aa2d3d1c89595c7eeb32407e6fdf5559
nfalyguisse <nfalyguisse72@gmail.com>
2024-06-02 13:24:14 +0200

    marques data-table in progress

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {MarquesComponent} from "./pages/marques/marques.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'marques', component:MarquesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/pages/marques/marques.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-marques',
+  templateUrl: './marques.component.html',
+  styleUrl: './marques.component.css'
+})
+export class MarquesComponent {
+
+}
